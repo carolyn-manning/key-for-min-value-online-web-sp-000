fbb6a1fa61e90c5e4a4704b3781071a53839b27b
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-07 03:55:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  value_array = []
   name_hash.collect do |key, value|
-    value_array = value
+    value_array << value
     value_array.min
-    key  
   end
+  key 
 end
