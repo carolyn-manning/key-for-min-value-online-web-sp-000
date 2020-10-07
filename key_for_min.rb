9bc4ef0be4f88efdc7038f58c50e0131b11f7170
Carolyn Manning <carolyn.manning3@gmail.com>
2020-10-07 04:06:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  name_hash.collect do |key, value|
-    if name_hash[:key] < value
+  name_hash.collect do |name, value|
+    if name_hash[:name] < value
       key
     end
   end
